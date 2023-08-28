daa3cec7cb792cd19df9774f6f7dab786ea4562c
HollandCasino <120786034+HollandCasino@users.noreply.github.com>
2023-08-28 23:24:01 +0200

    subscan.py
    Rewrite.

--- subscan.py
+++ subscan.py
@@ -2,10 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import httpx
 
-# https://github.com/HollandCasino
-# This script uses https://crt.sh/ to uncover both active and expired subdomains.
-# The status_code "-1" represent an error. 
-
 def fetch_html_content(url):
     response = requests.get(url)
     return response.text
@@ -18,41 +14,43 @@ def find_matching_domains(td_texts, domain):
     return printed_strings
 
 def prepare_urls(printed_strings):
-    urls = []
-    for td_text in printed_strings:
-        urls.append("http://" + td_text)
-        urls.append("https://" + td_text)
+
+    urls = set()
+    for x in printed_strings:
+        urls.add("http://" + x)
+        urls.add("https://" + x)
     return urls
 
-
-def check_status_codes(urls):
-    status_codes = []
-    for sc_check in urls:
+def sc_check(urls):
+    check_urls = list(urls)
+    
+    for x in check_urls:
         try:
-            check = httpx.get(sc_check, timeout=1)
-            status_codes.append((sc_check, check.status_code))
+            check = httpx.get(x)
+            print(f"{x}: {check.status_code}") 
         except httpx.RequestError as e:
-            status_codes.append((sc_check, -1))  
-    return status_codes
+            print(f"{x}: Error - {e}")
 
-def print_sorted_status_codes(status_codes):
-    status_codes.sort(key=lambda x: x[1])
-    for sc_check, status_code in status_codes:
-        print(f"{sc_check}: {status_code}")
+    return x, check.status_code
 
 
 def main():
-    domain = input("What is the domain? Example: google.nl  :   ")
+    
+    domain = input("What is the domain? Example: google.nl: ")
     url = "https://crt.sh/?q=" + domain
 
-    response = fetch_html_content(url)
-    soup = BeautifulSoup(response, "html.parser")
-    td_texts = [td.get_text() for td in soup.find_all("td")]
+    try:
+        response = fetch_html_content(url)
+        soup = BeautifulSoup(response, "html.parser")
+        td_texts = [td.get_text() for td in soup.find_all("td")]
+        printed_strings = find_matching_domains(td_texts, domain)
+        urls = prepare_urls(printed_strings)
+        sc_check(urls)
+    except requests.RequestException as e:
+        print("Error fetching or parsing data:", e)
+    except KeyboardInterrupt:
+        print("Operation interrupted by user.")
 
-    printed_strings = find_matching_domains(td_texts, domain)
-    urls = prepare_urls(printed_strings)
-    status_codes = check_status_codes(urls)
-    print_sorted_status_codes(status_codes)
 
 if __name__ == "__main__":
     main()
